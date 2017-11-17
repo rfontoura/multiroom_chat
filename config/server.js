a0013bfd8a8379e540310ea6f66be78b585323c3
Rafael Fontoura <rafael.fontoura@dataeasy.com.br>
2017-11-17 18:00:21 -0200

    Criação de estrutura inicial do projeto
    1. Importação do Express, Consign, Body Parser e Express Validators
    2. Configuração do consign para importar módulos e do Express Validator
    3. Configuração de diretório público de coisas estáticas
    4. Inclusão de views com CSS e layout
    5. Criação de controllers e routes

---
+++ config/server.js
@@ -0,0 +1,37 @@
+// importação do módulo do framework Express
+var express = require('express');
+
+// importação do módulo do Consign
+var consign = require('consign');
+
+// importação do módulo do body-parser
+var bodyParser = require('body-parser');
+
+// importação do módulo do Express validator
+var expressValidator = require('express-validator');
+
+// inicialização do Express
+var app = express();
+
+// configuração do EJS
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+// configuração do middleware express.static
+app.use(express.static('./app/public'));
+
+// configuração do middleware Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configuração do middleware Express validador
+app.use(expressValidator());
+
+// efetua auto-load das rotas, models e controllers para o objeto 'app'
+consign()
+    .include('app/routes')
+    .then('app/models')
+    .then('app/controllers')
+    .into(app);
+
+// exportação do objeto 'app' (Express)
+module.exports = app;
