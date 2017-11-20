a30a518178ace43e2e2de226bdde2f0f795d678b
Rafael Fontoura <rafael.fontoura@dataeasy.com.br>
2017-11-20 17:30:02 -0200

    Evolução das msgs e lista de usuários
    1. Utilizando socket.io para enviar mensagem do cliente (navegador) para servidor NodeJS, definindo nomes de mensagens
    2. Exibindo mensagens localmente e por broadcast para janela de mensagens de pessoas no chat
    3. Atualizando lista de usuários no chat e descendo barra de rolagem do chat conforme enviam mensagens

--- app.js
+++ app.js
@@ -10,11 +10,39 @@ var server = app.listen(3000, function () {
 // disponibilizada pelo NodeJS para o Express
 var io = require('socket.io').listen(server);
 
+app.set('io', io); // definindo variável global
+
 // criando conexão por websocket
 io.on('connection', function (socket) {
     console.log('Usuário conectou.');
 
     socket.on('disconnect', function () {
         console.log('Usuário desconectou.');
+    });
+
+    socket.on('msgParaServidor', function (data) {
+        // participantes
+        socket.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        // participantes
+        if (parseInt(data.apelido_atualizado_nos_clientes) == 0) {
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+    
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+        }
     });
 });
